111f9bef422904887629b967750d4a9a05d1f1de
Igor Tkachenko <igor.w.tkachenko@gmail.com>
2025-01-17 13:40:24 +0100

    Take already parsed authentication object for permission check

--- packages/server-oidc-rp/src/gate.ts
+++ packages/server-oidc-rp/src/gate.ts
@@ -3,10 +3,6 @@ import type { GateService } from '@owlmeans/module'
 import { OIDC_GATE } from '@owlmeans/oidc'
 import type { Config, Context } from './types.js'
 import { AuthForbidden } from '@owlmeans/auth'
-import type { Auth } from '@owlmeans/auth'
-import { extractAuthToken } from '@owlmeans/auth-common/utils'
-import { OIDC_WRAPPED_TOKEN } from '@owlmeans/oidc'
-import { EnvelopeKind, makeEnvelopeModel } from '@owlmeans/basic-envelope'
 import { createGateModel } from './model/gate.js'
 
 export const makeOidcGate = (alias: string = OIDC_GATE): GateService => {
@@ -15,16 +11,13 @@ export const makeOidcGate = (alias: string = OIDC_GATE): GateService => {
       await service.ready()
       const ctx = service.assertCtx<Config, Context>()
 
-      const token = extractAuthToken(req, OIDC_WRAPPED_TOKEN)
-      if (token == null) {
-        throw new AuthForbidden('token')
+      if (req.auth == null) {
+        throw new AuthForbidden('auth')
       }
-
-      const user = makeEnvelopeModel<Auth>(token, EnvelopeKind.Token).message()
 
       const model = createGateModel(ctx)
 
-      const permissions = await model.loadPermissions(user, params)
+      const permissions = await model.loadPermissions(req.auth, params)
 
       if (permissions.length < 1) {
         throw new AuthForbidden('permission')
