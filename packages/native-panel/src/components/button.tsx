de01b46faf2bf3e4ca98d18441a4c17bc1679ad9
Igor Tkachenko <igor.tkachenko@owlmeans.com>
2025-04-26 11:12:15 +0200

    Fixed RN outlined buttons

--- packages/native-panel/src/components/button.tsx
+++ packages/native-panel/src/components/button.tsx
@@ -7,9 +7,11 @@ import { Text } from './text.js'
 
 export const Button: FC<ButtonProps> = ({ icon, dark, name, onPress, color, textColor, textVariant, i18n, style, variant = "contained" }) => {
   const theme = useTheme()
-
   return <PaperButton mode={variant} dark={dark ?? true} onPress={onPress}
     buttonColor={color != null ? theme.colors[color as keyof typeof theme.colors] as any : undefined}
+    textColor={
+      textColor ? theme.colors[textColor as keyof typeof theme.colors] as string : undefined
+    }
     icon={icon} style={style}
   >
     <Text variant={textVariant ?? 'titleMedium'} i18n={i18n} color={
