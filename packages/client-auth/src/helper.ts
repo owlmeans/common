c23b91382e2d6edf6611a44f7b7ec89e3fea7109
Igor Tkachenko <igor.tkachenko@owlmeans.com>
2026-02-01 18:15:22 +0100

    Fixed useSelfAuth check

--- packages/client-auth/src/helper.ts
+++ packages/client-auth/src/helper.ts
@@ -1,4 +1,4 @@
-import { useContext } from '@owlmeans/client'
+import { useContext, useNavigate } from '@owlmeans/client'
 import type { ClientModule } from '@owlmeans/client-module'
 import { provideRequest } from '@owlmeans/client-module'
 import { useWs as useWebSocket } from '@owlmeans/client-socket'
@@ -6,10 +6,10 @@ import type { AbstractRequest } from '@owlmeans/module'
 import type { AuthServiceAppend } from './types.js'
 import { useEffect, useMemo, useState } from 'react'
 import type { ClientContext } from '@owlmeans/client-context'
-import { AUTH_QUERY } from '@owlmeans/auth'
-import { useFlow } from '@owlmeans/web-flow'
-import { DEFAULT_ENTITY } from './consts.js'
-import { OidcAuthStep } from '@owlmeans/flow'
+import { AUTH_QUERY, DISPATCHER } from '@owlmeans/auth'
+// import { useFlow } from '@owlmeans/web-flow'
+// import { DEFAULT_ENTITY } from './consts.js'
+// import { OidcAuthStep } from '@owlmeans/flow'
 
 export const useWs = (
   module: string | ClientModule<any>, _request?: Partial<AbstractRequest<any>>
@@ -41,22 +41,24 @@ export const useWs = (
   return useWebSocket(module, request)
 }
 
-export const useSelfAuth = (force: boolean = true, entity: string = DEFAULT_ENTITY) => {
+export const useSelfAuth = (force: boolean = true/*, entity: string = DEFAULT_ENTITY*/) => {
   const context = useContext() as unknown as AuthServiceAppend & ClientContext
-  const flow = useFlow(context.cfg.shortAlias ?? context.cfg.service)
+  // const flow = useFlow(context.cfg.shortAlias ?? context.cfg.service)
   const [authenticated, setAuthenticated] = useState(false)
+  const nav = useNavigate()
 
   useEffect(() => {
-    if (flow == null) return
+    // if (flow == null) return
     context.auth().authenticated().then(async auth => {
       if (force && !auth) {
-        flow.flow().entity(entity)
-        await flow.proceed(flow.flow().transition(OidcAuthStep.Ephemeral))
+        nav.go(DISPATCHER)
+        // flow.flow().entity(entity)
+        // await flow.proceed(flow.flow().transition(OidcAuthStep.Ephemeral))
       }
 
       setAuthenticated(!!auth)
     })
-  }, [flow])
+  }, [])
 
   return authenticated
 }
